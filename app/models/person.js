007d8d3f4fa180ae1747337dbc04e886ff95c14b
Ramiro Franco <me@ramiro.mx>
2021-05-19 14:20:04 -0700

    Adding fauna.. I hope

--- app/models/person.js
+++ app/models/person.js
@@ -1,7 +1,7 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class PersonModel extends Model {
-  @attr('boolean') completed;
-  @attr('boolean') active;
-  @attr('string') name;
+  @attr completed;
+  @attr active;
+  @attr name;
 }
