b145b7a44cc98575d713da32d38b3af0774ab68e
Ramiro Franco <me@ramiro.mx>
2021-05-20 14:14:35 -0700

    Telling store to create record for person

--- app/controllers/person/new.js
+++ app/controllers/person/new.js
@@ -1,10 +1,22 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class PersonNewController extends Controller {
+  @service router;
+  @service store;
   @tracked name = '';
 
   get hasNoName() {
     return !this.name;
+  }
+
+  @action
+  savePerson(event) {
+    event.preventDefault();
+    let { name } = this;
+    let person = this.store.createRecord('person', { name });
+    person.save().then(this.router.transitionTo('index'));
   }
 }
