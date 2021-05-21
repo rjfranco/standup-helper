14a7244299c2ababd4ec3e1c16e7c5f0ef54e8fc
Ramiro Franco <me@ramiro.mx>
2021-05-21 09:23:49 -0700

    start adding group model

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
   @service store;
-  model() {
-    return this.store.findAll('person');
+  model(params) {
+    return this.store.findRecord('group', params.group_name);
   }
 }
