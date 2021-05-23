ad13f5bff0771c547d7439677980de84e04b0e9c
Ramiro Franco <me@ramiro.mx>
2021-05-22 23:16:12 -0700

    Starting down the path of adding group service

--- app/routes/group/show.js
+++ app/routes/group/show.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class GroupShowRoute extends Route {
+  @service store;
+
+  model(params) {
+    return this.store.findRecord('group', params.group_name);
+  }
 }
