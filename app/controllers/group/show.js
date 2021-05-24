75d76f58d15c7613725123e69c1de498677197fa
Ramiro Franco <me@ramiro.mx>
2021-05-23 23:27:08 -0700

    Add sorted list based on model

--- app/controllers/group/show.js
+++ app/controllers/group/show.js
@@ -1,7 +1,19 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { sort } from '@ember/object/computed';
 
 export default class GroupShowController extends Controller {
+  @sort('model.people', function(a, b) {
+    if (a.name > b.name) {
+      return 1;
+    } else if (b.name > a.name) {
+      return -1;
+    }
+
+    return 0;
+  })
+  alphaPeople;
+
   @action
   deletePerson(person) {
     let { model: group } = this;
