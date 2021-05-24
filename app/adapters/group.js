a2ef354f0aedea12180cf54ed8251d4b266eafde
Ramiro Franco <me@ramiro.mx>
2021-05-24 09:29:39 -0700

    Starting the streaming process

--- app/adapters/group.js
+++ app/adapters/group.js
@@ -4,6 +4,8 @@ import { inject as service } from '@ember/service';
 export default class GroupAdapter extends ApplicationAdapter {
   @service fauna;
 
+  watchList = {};
+
   titleize(str) {
     return str.replace(/^[a-z]|-[a-z]/g, (str) => str.toUpperCase());
   }
@@ -23,17 +25,14 @@ export default class GroupAdapter extends ApplicationAdapter {
   queryRecord(store, type, { slug }) {
     let { Get, Index, Lambda, Map, Match, Paginate, Var } = faunadb.query;
     let name = this.titleize(slug);
+    let query = Match(Index('group_by_name'), name);
     return this.fauna.client
-      .query(
-        Map(
-          Paginate(Match(Index('group_by_name'), name)),
-          Lambda('X', Get(Var('X')))
-        )
-      )
-      .then((response) => ({
-        ...response.data[0].data,
-        id: response.data[0].ref.value.id,
-      }));
+      .query(Map(Paginate(query), Lambda('X', Get(Var('X')))))
+      .then((response) => {
+        let id = response.data[0].ref.value.id;
+        this.watchRecord(name, id);
+        return { ...response.data[0].data, id };
+      });
   }
 
   updateRecord(store, type, snapshot) {
@@ -47,5 +46,28 @@ export default class GroupAdapter extends ApplicationAdapter {
         })
       )
       .then((response) => ({ ...response.data, id: response.ref.id }));
+  }
+
+  watchRecord(name, id) {
+    if (this.watchList[name]) return;
+
+    let { Collection, Ref } = faunadb.query;
+    let docRef = Ref(Collection('groups', id));
+    this.watchList[name] = this.fauna.client.stream
+      .document(docRef)
+      .on('snapshot', (snapshot) => {
+        console.log('Snapshot came in:', snapshot);
+      })
+      .on('version', (version) => {
+        console.log('version came in:', version);
+      })
+      .on('error', (error) => {
+        console.log('Error:', error);
+        this.watchList[name].close();
+        setTimeout(() => {
+          this.watchRecord(name, docRef);
+        }, 1000);
+      })
+      .start();
   }
 }
